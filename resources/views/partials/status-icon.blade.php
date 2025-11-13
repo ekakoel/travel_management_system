e046df73f8b47e4fadc5a7b38ce340dc6b7e0502
Eka Koel <guseka80@gmail.com>
2025-11-13 15:40:33 +0800

    bug fix

--- resources/views/partials/status-icon.blade.php
+++ resources/views/partials/status-icon.blade.php
@@ -1,15 +1,15 @@
 
 @if ($status == "Rejected")
-    <div class="status-rejected"></div>
+    <div class="status-rejected">{{ $status }}</div>
 @elseif ($status == "Invalid")
-    <div class="status-invalid"></div>
+    <div class="status-invalid">{{ $status }}</div>
 @elseif ($status == "Active")
-    <div class="status-active"></div>
+    <div class="status-active">{{ $status }}</div>
 @elseif ($status == "Waiting")
-    <div class="status-waiting"></div>
+    <div class="status-waiting">{{ $status }}</div>
 @elseif ($status == "Draft")
-    <div class="status-draft"></div>
+    <div class="status-draft">{{ $status }}</div>
 @elseif ($status == "Archived")
-    <div class="status-archived"></div>
+    <div class="status-archived">{{ $status }}</div>
 @else
 @endif
