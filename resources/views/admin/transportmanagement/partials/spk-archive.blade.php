2d0731e51987e97279de0248e966397f648a5ca0
Eka Koel <guseka80@gmail.com>
2026-09-23 11:18:17 +0800

    update

--- resources/views/admin/transportmanagement/partials/spk-archive.blade.php
+++ resources/views/admin/transportmanagement/partials/spk-archive.blade.php
@@ -18,6 +18,9 @@
                     </td>
                     <td>
                         <strong>{{ $spk->order_number ?? '-' }}</strong>
+                        @if ($spk->operator != null)
+                            <span>{{ $spk->operator?->name.' ('.$spk->operator?->code.')' }}</span>
+                        @endif
                         <span>{{ $spk->type ?? '-' }} / {{ trans_choice('transport-management.table.pax', (int) $spk->number_of_guests, ['count' => (int) $spk->number_of_guests]) }}</span>
                     </td>
                     <td>
